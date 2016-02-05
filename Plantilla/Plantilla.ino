26bf3c060e0f73b2f64e867ca7542333de1e5488
janroig <jancore10@gmail.com>
2016-02-05 02:05:29 +0100

    j

--- Plantilla/Plantilla.ino
+++ Plantilla/Plantilla.ino
@@ -1,23 +1,41 @@
-/***************************************************************
-**                                                            **
-**                 Descripció del programa                    **                             
-**                                                            **
-***************************************************************/
+/**********************************************************************************************************
+**                                          Blink                                                        **
+**             Turns on an LED on for one second, then off for one second, repeatedly.                   **                             
+**                                                                                                       **
+***********************************************************************************************************/
 
 //******  Includes  ********************************************
 
 
+  Most Arduinos have an on-board LED you can control. On the Uno and
+  Leonardo, it is attached to digital pin 13. If you're unsure what
+  pin the on-board LED is connected to on your Arduino model, check
+  the documentation at http://www.arduino.cc
+
+  This example code is in the public domain.
+
 //******  Variables  *******************************************
 
 
+  modified 8 May 2014
+  by Scott Fitzgerald
+ */
+
 //******  Setup  ***********************************************
-void setup(){
-  
+
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
 }
 
 
 //******  Loop  ***********************************************
-void loop(){
-  
+
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
 }
+
 
